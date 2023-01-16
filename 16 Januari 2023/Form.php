299ab5ef4af3ce9121b3b1c538ab1f188ea53404
BjorRiz <mfarizalhaq1903@gmail.com>
2023-01-16 11:28:13 +0700

    PHP

---
+++ 16 Januari 2023/Form.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>FORM PHP</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+
+    <h1>BUKU TAMU</h1>
+    <hr>
+
+    <form action="Proses.php" method="POST">
+        Nama : <input type="text" name="nama">
+        <br>
+        Email : <input type="email" name="email">
+        <br>
+        Pesan : <textarea name="pesan"></textarea>
+        <br><br>
+        <input type="submit" value="simpan">
+    </form>
+
+   
+
+    </body>
+</html>
---
+++ 16 Januari 2023/Proses.php
@@ -0,0 +1,4 @@
+<?php
+echo $_POST['nama'];
+// echo $_POST['pesan'];
+?>
